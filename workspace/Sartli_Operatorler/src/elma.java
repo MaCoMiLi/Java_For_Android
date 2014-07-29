dfaea527ae87f14be877c305ddc29348c5888716
“MaCoMiLi” <314anil314@gmail.com>
2014-07-29 18:01:09 +0300

    şartlı operatörler

---
+++ workspace/Sartli_Operatorler/src/elma.java
@@ -0,0 +1,13 @@
+
+public class elma {
+	
+	public static void main(String args[]){
+		
+		Integer yas = 40;
+		System.out.println(yas < 45 ? "daha gençsin" : "yaşlanmısşın moruk");
+		
+	}
+	
+	
+
+}
