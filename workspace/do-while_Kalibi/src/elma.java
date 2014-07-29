496fc398d231bfb1ef94a513fd040b38d38718c9
“MaCoMiLi” <314anil314@gmail.com>
2014-07-29 19:31:54 +0300

    do-while kalıbı

---
+++ workspace/do-while_Kalibi/src/elma.java
@@ -0,0 +1,20 @@
+
+public class elma {
+	
+	public static void main(String args[]){
+		
+		int sayac=12;
+		
+		do
+		{
+		
+			System.out.println(sayac);
+			sayac+=3;
+			
+		}
+		
+		while(sayac < 50);
+		
+	}
+
+}
