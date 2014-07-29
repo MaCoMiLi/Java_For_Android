943463361d311b453f5c8825a638c2627b820531
“MaCoMiLi” <314anil314@gmail.com>
2014-07-29 22:24:18 +0300

    diziler2

---
+++ workspace/Diziler2/src/elma.java
@@ -0,0 +1,17 @@
+
+public class elma {
+
+	public static void main(String args[]){
+		
+		System.out.println("Sıra\tSayfa No"); // \t ifadesi ayırmak için kullanılır.
+		int dizi[] = {21,56,75,899,321};
+		
+		for(int sayac = 0; sayac < dizi.length; sayac++){
+			
+			System.out.println(sayac + "\t" + dizi[sayac]);
+			
+		}
+		
+	}
+	
+}
